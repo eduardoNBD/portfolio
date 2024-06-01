508e0bd0b3a47adc9ace55f9033fa631db4e0a17
Eduardo <lalo_elma@hotmail.com>
2024-06-01 16:04:00 -0600

    dark mode

--- src/constants/arrayData.ts
+++ src/constants/arrayData.ts
@@ -2,6 +2,7 @@ export interface Event {
     title: string;
     date: string;
     description: Array<string>;
+    places: Array<string>;
 }
 
 export const events: Event[] = [
@@ -10,13 +11,19 @@ export const events: Event[] = [
         date: 'Noviembre 2019 - Actualmente',  
         description: [
             'Encargado del diseño, desarrollo y back-end de aplicaciones web y soluciones tecnológicas tanto internas como externas.',  
-            'Encargado de administrar de servidores y su mantenimiento'
+            'Encargado de administrar de servidores y su mantenimiento'],
+        places:[
+            'Green Shield Technology'
         ]
+
     },
     { 
         title: 'Diseñador Web',
         date: 'Mayo 2017 - Noviembre 2019',
-        description: ['Encargado del desarrollo de sitios web y su mantenimiento']
+        description: ['Encargado del desarrollo de sitios web y su mantenimiento'],
+        places:[
+            'Green Shield Technology'
+        ]
     },
     { 
         title: 'Programador Freelancer',
@@ -25,6 +32,12 @@ export const events: Event[] = [
             "Desarrollo de aplicaciones web, PWA y móviles (Android e IOS), desde del front-end hasta el back-end.",
             "Encargado de administrar de servidores y su mantenimiento.",
             "Gestión de servicios VOIP, WEB y CRON",
+        ],
+        places:[
+            'Contralinea',
+            'MobileSail',
+            'NoticiasAl',
+            'Encanta Musica'
         ]
     }
 ];
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: "media",
+	darkMode: "class",
 	theme: {
 		extend: {
 			keyframes: {
 				height: {
 				  '0%': { height: '0%' },
-				  '100%': { height: '100%' },
+				  '90%': { opacity: "0.9"},
+				  '100%': { height: '100%', opacity:"0" },
 				},
 			},
 			animation: {
