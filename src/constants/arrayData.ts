2d52d9eb4f323a1ff25e6146266dc962ea330ddb
Eduardo <lalo_elma@hotmail.com>
2024-06-03 18:45:27 -0600

    projects

--- src/constants/arrayData.ts
+++ src/constants/arrayData.ts
@@ -5,6 +5,15 @@ export interface Event {
     places: Array<string>;
 }
 
+export interface Project {
+    title: string;
+    img: string;
+    description: Array<string>;
+    technologies: Array<string>;
+    type:number;
+    url?:string;
+}
+
 export const events: Event[] = [
     { 
         title: 'Lead Developer',
@@ -15,7 +24,7 @@ export const events: Event[] = [
         places:[
             'Green Shield Technology'
         ]
-
+ 
     },
     { 
         title: 'Diseñador Web',
@@ -41,3 +50,43 @@ export const events: Event[] = [
         ]
     }
 ];
+
+export const projects: Project[] = [
+    {
+        title: 'Admin Dashboard', 
+        img: "admindashboard.png",
+        description: [
+            "AdminDashboard es un panel de administración adaptable a cualquier dispositivo, utilizado para el registro de citas, venta de servicios y productos, junto con su registro de ventas y generación de recibos en PDF.",
+            "Gestión de roles y privilegios dentro de las funciones del sistema.", 
+        ],
+        technologies: [
+            "Laravel",
+            "TailwindCSS", 
+        ],
+        type: 0
+    },
+    {
+        title: 'Automatización de tareas con puppeteer', 
+        img: "puppeteer.png",
+        description: [
+            "App para prueba de automatización par agragar tareas de trello a todoist.", 
+        ],
+        technologies: [
+            "NodeJS", 
+        ],
+        type:1,
+        url: "https://github.com/eduardoNBD/puppeteerTodoist"
+    },
+    {
+        title: 'Mita Eats Cliente y Repartidor', 
+        img: "mitaeats.png",
+        description: [
+            "App desarrollada para IOS y Android empleada para venta y repartición de comida y productos varios.",
+            "Permite a usuarios solicitar productos dentro del area de Punta de mita.", 
+        ],
+        technologies: [
+            "React Native", 
+        ],
+        type:0
+    },
+]
