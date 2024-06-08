5dad07acd349725d39a84d8cc73d15b75a97752c
Eduardo <lalo_elma@hotmail.com>
2024-06-07 20:27:42 -0600

    desplegar en vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   integrations: [tailwind()]
 });
